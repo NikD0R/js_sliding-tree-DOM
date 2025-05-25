5b28eb3f1b44a3be834ff4628741d021a91d8918
Nikita Doroshenko <nikitokd05@gmail.com>
2025-05-25 13:36:32 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((elem) => {
+  const children = elem.childNodes;
+
+  for (let i = 0; i < children.length; i++) {
+    const node = children[i];
+
+    if (node.nodeType === 1 && node.tagName === 'UL') {
+      const span = document.createElement('span');
+
+      for (let j = 0; j < i; j++) {
+        span.appendChild(children[0]);
+      }
+
+      span.addEventListener('click', () => {
+        if (node.style.display === 'none') {
+          node.style.display = '';
+        } else {
+          node.style.display = 'none';
+        }
+      });
+
+      elem.insertBefore(span, node);
+      break;
+    }
+  }
+});
